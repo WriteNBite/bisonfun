7060593b8320c96520db4de170ea15885079706a
Alexander <writenbite@protonmail.com>
2023-06-18 23:04:05 +0300

    Create ImageService to get Images instead of raw using RedisTemplate;

--- src/main/java/com/bisonfun/web/HomeController.java
+++ src/main/java/com/bisonfun/web/HomeController.java
@@ -4,15 +4,13 @@ import com.bisonfun.model.VideoEntertainment;
 import com.bisonfun.client.anilist.AniListClient;
 import com.bisonfun.client.tmdb.TmdbClient;
 import com.bisonfun.client.anilist.TooManyAnimeRequestsException;
+import com.bisonfun.service.redis.ImageService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
-import java.util.ArrayList;
 import java.util.List;
-import java.util.Objects;
 import java.util.Random;
 
 @Controller
@@ -23,20 +21,20 @@ public class HomeController {
     final
     TmdbClient tmdbClient;
     final
-    RedisTemplate<String, String> redisTemplate;
+    ImageService imageService;
 
     @Autowired
-    public HomeController(AniListClient aniListClient, TmdbClient tmdbClient, RedisTemplate<String, String> redisTemplate) {
+    public HomeController(AniListClient aniListClient, TmdbClient tmdbClient, ImageService imageService) {
         this.aniListClient = aniListClient;
         this.tmdbClient = tmdbClient;
-        this.redisTemplate = redisTemplate;
+        this.imageService = imageService;
     }
 
     @GetMapping("/")
     public String home(Model model) throws TooManyAnimeRequestsException {
         Random random = new Random();
 
-        List<String> backgrounds = new ArrayList<>(Objects.requireNonNull(redisTemplate.opsForSet().members("main-page-background")));
+        List<String> backgrounds = imageService.getMainPageBackground();
 
         List<VideoEntertainment> animeTrends = aniListClient.parseAnimeTrends();
         List<VideoEntertainment> movieTrends = tmdbClient.parseMovieTrends();

5cbdb7b21beba0ed3ca24000fb60fa66b58c90b0
Alexander <writenbite@protonmail.com>
2023-07-30 01:08:17 +0300

    Implement VideoEntertainment Serializer Implement VideoEntertainmentSerializer for better serialization; Register VideoEntertainmentSerializer and set standard date format;

--- src/main/java/com/bisonfun/config/GsonConfig.java
+++ src/main/java/com/bisonfun/config/GsonConfig.java
@@ -6,6 +6,8 @@ import com.bisonfun.deserializer.TmdbTvShowDeserializer;
 import com.bisonfun.model.AniAnime;
 import com.bisonfun.model.TMDBMovie;
 import com.bisonfun.model.TMDBTVShow;
+import com.bisonfun.model.VideoEntertainment;
+import com.bisonfun.serializer.VideoEntertainmentSerializer;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import org.springframework.context.annotation.Bean;
@@ -16,9 +18,11 @@ public class GsonConfig {
     @Bean
     public Gson gson(){
         return new GsonBuilder()
+                .setDateFormat("yyyy-MM-dd")
                 .registerTypeAdapter(TMDBMovie.class, new TmdbMovieDeserializer())
                 .registerTypeAdapter(TMDBTVShow.class, new TmdbTvShowDeserializer())
                 .registerTypeAdapter(AniAnime.class, new AniAnimeDeserializer())
+                .registerTypeAdapter(VideoEntertainment.class, new VideoEntertainmentSerializer())
                 .create();
     }
 }
